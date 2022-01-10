c05050062c0829522eea3ba4de112950db76d4c8
azmasamy <azmasamy173@gmail.com>
2022-01-10 20:26:04 +0300

    first commit

---
+++ product_label_dymo/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Product Label Dymo',
+    'version': '1.2',
+    'category': 'Sales/Sales',
+    'depends': ['product'],
+    'description': "This module allows users to print dymo product labels",
+    'data': [
+        'data/report_paperformat.xml',
+        'report/product_reports_dymo.xml',
+        'report/product_product_templates_dymo.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ product_label_dymo/models/product_label_dymo.py
@@ -0,0 +1,6 @@
+from odoo import models
+
+
+class ProductLabelDymo(models.Model):
+    _name = "product.label.dymo"
+    _description = "print a product label dymo"
